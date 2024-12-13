eaf0029930350407cf563b250479ca2defded784
ersu-amine <ersu-amine@gmail.com>
2024-12-12 23:13:25 -0500

    test(registration/login): complete test for valid and invalid login  with toast message

--- src/test/java/com/appium/tests/ecommerceapp/RegistrationForm.java
+++ src/test/java/com/appium/tests/ecommerceapp/RegistrationForm.java
@@ -1,14 +1,12 @@
 package com.appium.tests.ecommerceapp;
 
-import com.appium.utilities.CommonLibrary;
+
 import com.appium.utilities.ECommerceAppBase;
+import com.appium.utilities.ECommerceUtils;
 import io.appium.java_client.AppiumBy;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class RegistrationForm extends ECommerceAppBase {
 
@@ -16,9 +14,8 @@ public class RegistrationForm extends ECommerceAppBase {
     public void validName() {
         //tap on the dropdown to open
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-        //TODO implement wait?
         // scroll until seeing target country
-        CommonLibrary.scrollIntoView("Canada");
+        ECommerceUtils.scrollIntoView("Canada");
         //select country
         driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id='android:id/text1' and @text='Canada']")).click();
         //enter name to input box
@@ -31,13 +28,17 @@ public class RegistrationForm extends ECommerceAppBase {
         driver.findElement(AppiumBy.className("android.widget.Button")).click();
 
         //assertion logining into app
+        //Products header is seen after successful login
+        Assert.assertEquals("Products",driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")).getText());
 
     }
 
     @Test
     public void spaceAsName() {
+        //tap on the dropdown to open
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
         // scroll until seeing target country
-        CommonLibrary.scrollIntoView("Canada");
+        ECommerceUtils.scrollIntoView("Canada");;
         //select country
         driver.findElement(By.xpath("//android.widget.TextView[@resource-id='android:id/text1' and @text='Canada']")).click();
         //enter name to input box
@@ -47,19 +48,23 @@ public class RegistrationForm extends ECommerceAppBase {
         //tap let's shop button
         driver.findElement(By.className("android.widget.Button")).click();
 
-        //assertion of error message
+        //assertion of toast message error
+        String toastMessage = driver.findElement(AppiumBy.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+        Assert.assertEquals("Toast message is NOT as expected","Please enter your name", toastMessage);
 
     }
 
 
     @Test
     public void numberAsName() {
+        //tap on the dropdown to open
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
         // scroll until seeing target country
-        CommonLibrary.scrollIntoView("Canada");
+        ECommerceUtils.scrollIntoView("Canada");
         //select country
         driver.findElement(By.xpath("//android.widget.TextView[@resource-id='android:id/text1' and @text='Canada']")).click();
         //enter name to input box
-        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("");
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("12345");
         //select gender
         driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
         //tap let's shop button
@@ -67,6 +72,7 @@ public class RegistrationForm extends ECommerceAppBase {
 
         //expected error message but logins
         // assertion of error message
+        Assert.assertEquals("Products",driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")).getText());
 
     }
 }
