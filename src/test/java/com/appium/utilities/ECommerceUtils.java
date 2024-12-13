c0d2dd369aaca96be6f81f3e900102d841f52fdf
ersu-amine <ersu-amine@gmail.com>
2024-12-12 23:14:37 -0500

    chore(library): add scroll method to ECommerceUtils

--- src/test/java/com/appium/utilities/ECommerceUtils.java
+++ src/test/java/com/appium/utilities/ECommerceUtils.java
@@ -1,5 +1,13 @@
 package com.appium.utilities;
 
-public class ECommerceUtils {
+import io.appium.java_client.AppiumBy;
+
+public class ECommerceUtils extends ECommerceAppBase{
+    /**
+     * @param elementText scrolls down until element text is in view
+     */
+    public static void scrollIntoView(String elementText) {
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"));"));
+    }
 
 }
