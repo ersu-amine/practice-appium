9f96f43683c183f6a31f6c5782c3f7f7159ac1e1
ersu-amine <ersu-amine@gmail.com>
2024-12-23 01:40:47 -0500

    chore(library): add method for page title verification

--- src/test/java/com/appium/utilities/CommonLibrary.java
+++ src/test/java/com/appium/utilities/CommonLibrary.java
@@ -2,6 +2,7 @@ package com.appium.utilities;
 
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
@@ -74,5 +75,9 @@ public class CommonLibrary extends BaseTest {
         js.executeScript("mobile: startActivity", ImmutableMap.of(
                 "intent", activity
         ));
+    }
+
+    public static void confirmPageTitle(String expectedTitle){
+        Assert.assertEquals(expectedTitle,driver.getTitle());
     }
 }
