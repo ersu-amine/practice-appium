4cd28c177a658d9cef72ec186aabd57620b4fbce
ersu-amine <ersu-amine@gmail.com>
2024-12-23 02:10:02 -0500

    feat(test): add method for google mobile page keyword search using google chrome mobile

--- src/test/java/com/appium/tests/mobile_browser/MobileBrowser.java
+++ src/test/java/com/appium/tests/mobile_browser/MobileBrowser.java
@@ -4,10 +4,12 @@ import com.appium.utilities.CommonLibrary;
 import com.appium.utilities.MobileBrowserTestBase;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 public class MobileBrowser extends MobileBrowserTestBase {
+    //TODO , driver is null when going with steps
     @Given("user is on the google homepage")
     public void user_is_on_the_google_homepage() {
         driver.get("https://www.google.com");
@@ -22,5 +24,14 @@ public class MobileBrowser extends MobileBrowserTestBase {
     @Then("user should see relevant results for {string}")
     public void user_should_see_relevant_results_for(String searchTerm) {
         CommonLibrary.confirmPageTitle(searchTerm);
+    }
+
+
+    @Test //works because each step in one method
+    public void test() throws InterruptedException {
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("pigment of reality" + Keys.ENTER);
+        Thread.sleep(3000);
+
     }
 }
