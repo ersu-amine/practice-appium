5d93b61f3bd61aebf524318b5a6a2ad8804d0608
ersu-amine <ersu-amine@gmail.com>
2024-12-23 01:40:17 -0500

    feat(stepdefs): add step definitions for search functionality

---
+++ src/test/java/com/appium/tests/mobile_browser/MobileBrowser.java
@@ -0,0 +1,26 @@
+package com.appium.tests.mobile_browser;
+
+import com.appium.utilities.CommonLibrary;
+import com.appium.utilities.MobileBrowserTestBase;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class MobileBrowser extends MobileBrowserTestBase {
+    @Given("user is on the google homepage")
+    public void user_is_on_the_google_homepage() {
+        driver.get("https://www.google.com");
+    }
+
+    @Given("user searches for {string}")
+    public void user_searches_for(String searchTerm) throws InterruptedException {
+        driver.findElement(By.name("q")).sendKeys("pigment of reality" + Keys.ENTER);
+        Thread.sleep(3000);
+    }
+
+    @Then("user should see relevant results for {string}")
+    public void user_should_see_relevant_results_for(String searchTerm) {
+        CommonLibrary.confirmPageTitle(searchTerm);
+    }
+}
