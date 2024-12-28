490b350b8c482b8c305904e4b91da1d68dc2ae72
ersu-amine <ersu-amine@gmail.com>
2024-12-27 22:29:36 -0500

    chore(testbase): add driver initialization into static block
    Driver setUp method with BeforeClass annotation was leading to null driver in step definitons

--- src/test/java/com/appium/utilities/MobileBrowserTestBase.java
+++ src/test/java/com/appium/utilities/MobileBrowserTestBase.java
@@ -13,13 +13,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
 
-public class MobileBrowserTestBase {
+public class MobileBrowserTestBase extends CommonLibrary{
 
     public static AppiumDriverLocalService service;
     public static AndroidDriver driver;
-
-    @BeforeClass
-    public static void setUpAppium() throws URISyntaxException, MalformedURLException {
+    static {
         service = new AppiumServiceBuilder().withAppiumJS(new File("/Users/michealthonton/.npm-global/lib/node_modules/appium/build/lib/main.js")).withIPAddress("127.0.0.1").usingPort(4723).build(); //http not required when giving as IP
         service.start();
 
@@ -28,9 +26,29 @@ public class MobileBrowserTestBase {
         options.setChromedriverExecutable("/Users/michealthonton/Documents/chromedriver-mac-x64/chromedriver");
         options.setCapability("browserName", "Chrome");
 
-        driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+        try {
+            driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
+//    @BeforeClass
+//    public static void setUpAppium() throws URISyntaxException, MalformedURLException {
+//        service = new AppiumServiceBuilder().withAppiumJS(new File("/Users/michealthonton/.npm-global/lib/node_modules/appium/build/lib/main.js")).withIPAddress("127.0.0.1").usingPort(4723).build(); //http not required when giving as IP
+//        service.start();
+//
+//        UiAutomator2Options options = new UiAutomator2Options();
+//        options.setDeviceName("Pixel 9 API 35");
+//        options.setChromedriverExecutable("/Users/michealthonton/Documents/chromedriver-mac-x64/chromedriver");
+//        options.setCapability("browserName", "Chrome");
+//
+//        driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//    }
 
     @AfterClass
     public static void tearDown() throws InterruptedException {
