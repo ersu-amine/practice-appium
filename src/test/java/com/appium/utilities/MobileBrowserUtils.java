ff5d0e4cf3c2663eeb085608dd74d6cea11e35ee
ersu-amine <ersu-amine@gmail.com>
2024-12-28 00:16:07 -0500

    feat(utils): add MobileBrowserUtils

---
+++ src/test/java/com/appium/utilities/MobileBrowserUtils.java
@@ -0,0 +1,8 @@
+package com.appium.utilities;
+
+import org.junit.Assert;
+
+public class MobileBrowserUtils extends MobileBrowserTestBase{
+    //extending to test base to get the driver access
+
+}
