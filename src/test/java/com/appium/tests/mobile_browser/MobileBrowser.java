ef8fcb38903971f7b1148b76b792f1952793949e
ersu-amine <ersu-amine@gmail.com>
2024-12-28 00:18:23 -0500

    feat(test): add neetcode testing practice

--- src/test/java/com/appium/tests/mobile_browser/MobileBrowser.java
+++ src/test/java/com/appium/tests/mobile_browser/MobileBrowser.java
@@ -1,11 +1,15 @@
 package com.appium.tests.mobile_browser;
 
+import com.appium.pages.NeetCodeHomePage;
 import com.appium.utilities.CommonLibrary;
 import com.appium.utilities.MobileBrowserTestBase;
+import com.appium.utilities.MobileBrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
 public class MobileBrowser extends MobileBrowserTestBase {
@@ -17,21 +21,33 @@ public class MobileBrowser extends MobileBrowserTestBase {
 
     @Given("user searches for {string}")
     public void user_searches_for(String searchTerm) throws InterruptedException {
-        driver.findElement(By.name("q")).sendKeys("pigment of reality" + Keys.ENTER);
+        driver.findElement(By.name("q")).sendKeys(searchTerm + Keys.ENTER);
         Thread.sleep(3000);
     }
 
     @Then("user should see relevant results for {string}")
     public void user_should_see_relevant_results_for(String searchTerm) {
-        CommonLibrary.confirmPageTitle(searchTerm);
+        //MobileBrowserUtils.confirmPageTitle(searchTerm);
     }
 
 
-    @Test //works because each step in one method
-    public void test() throws InterruptedException {
+    @Test
+    public void testGoogle() throws InterruptedException {
         driver.get("https://www.google.com");
         driver.findElement(By.name("q")).sendKeys("pigment of reality" + Keys.ENTER);
         Thread.sleep(3000);
 
+    }
+
+    @Test
+    public void testNeetCode() throws InterruptedException {
+        //configuration of driver with appium but interaction with browser is with selenium
+        driver.get("https://neetcode.io");
+        //scroll, inject javascript
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,800)", "");
+        NeetCodeHomePage page = new NeetCodeHomePage();
+        String buttonText = page.featureRequestsButton.getText();
+        Assert.assertEquals("Text of button is not as expected","Feature Requests",buttonText);
     }
 }
